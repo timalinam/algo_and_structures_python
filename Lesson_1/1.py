4e150ad9a7224f91cb63750f8f380efd31afe554
timalinam <p.paradise93@gmail.com>
2019-12-26 13:49:32 +0300

    измнения в 1м файле

--- Lesson_1/1.py
+++ Lesson_1/1.py
@@ -3,6 +3,6 @@
 number = int(input('Введите трехзначное число\n'))
 sum_numbers = (number % 10) + ((number // 10) % 10) + (number // 100)
 pr_numbers = (number % 10) * ((number // 10) % 10) * (number // 100)
+
 print(f'сумма цифр числа {number} равна {sum_numbers}')
 print(f'произведение цифр числа {number} равно {pr_numbers}')
-
