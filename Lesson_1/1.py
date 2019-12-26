60b8b97a85ed452335389083ddb12a32417f29c0
timalinam <p.paradise93@gmail.com>
2019-12-26 13:59:32 +0300

    1234

--- Lesson_1/1.py
+++ Lesson_1/1.py
@@ -9,4 +9,3 @@ pr_numbers = (number % 10) * ((number // 10) % 10) * (number // 100)
 
 print(f'сумма цифр числа {number} равна {sum_numbers}')
 print(f'произведение цифр числа {number} равно {pr_numbers}')
-
