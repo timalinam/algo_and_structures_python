af93c8f4da1faf32787cbcc69f99a2a6dffc2ed2
timalinam <p.paradise93@gmail.com>
2020-02-01 20:39:47 +0300

    Сделано дз7

--- Lesson_7/1.py
+++ Lesson_7/1.py
@@ -4,3 +4,23 @@
 исходный и отсортированный массивы. Сортировка должна быть реализована в
 виде функции. По возможности доработайте алгоритм (сделайте его умнее).
 """
+import random
+
+
+def bubble(random_list):
+    n = 1
+    list_is_sorted = True
+    while n < len(random_list):
+        for i in range(len(random_list) - n):
+            if random_list[i] < random_list[i + 1]:
+                random_list[i], random_list[i + 1] = random_list[i + 1], random_list[i]
+                list_is_sorted = False
+        if list_is_sorted:
+            break
+        n += 1
+    return random_list
+
+
+r_list = [random.randint(-100, 100) for _ in range(10)]
+print(r_list)
+print(bubble(r_list))
